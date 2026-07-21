2d8046de391ae542c9950c9dcefa42407894b927
Aditya Lour <louraditya6584@gmail.com>
2026-07-22 02:29:32 +0530

    Add Logout route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -128,3 +128,13 @@ export const login = async (req, res) => {
         res.status(500).json({ message: 'Login failed', error: error.message });
     }
 }
+
+export const logout = (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).json({ message: 'Logout failed' });
+        }
+        res.clearCookie('connect.sid');
+        res.status(200).json({ message: 'Logged out successfully' });
+    });
+};
